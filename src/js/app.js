ab4cd406d92c7c4c09da7745efc380a734e7bf9a
Xhall1 <gridoracones@gmail.com>
2025-08-07 12:10:38 -0500

    feat: adding Modal

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     createGallery();
 })
 
@@ -6,11 +6,54 @@ function createGallery() {
     const gallery = document.querySelector('.gallery-images');
     const imageNumber = 16;
 
-    for(let i = 1; i <= imageNumber; i++){
+    for (let i = 1; i <= imageNumber; i++) {
         const image = document.createElement('IMG');
         image.src = `src/img/gallery/full/${i}.jpg`;
         image.alt = `Gallery Image`;
 
-        gallery.appendChild(image)
+        // Event Handler 
+        image.onclick = function () {
+            showImage(i)
+        }
+
+        gallery.appendChild(image);
     }
+}
+
+function showImage(i) {
+
+    const image = document.createElement('IMG');
+    image.src = `src/img/gallery/full/${i}.jpg`;
+    image.alt = `Gallery Image`;
+
+    //Modal
+    const modal = document.createElement('DIV');
+    modal.classList.add('modal');
+    modal.onclick = closeModal;
+
+    // Button to close Modal
+    const closeModalBtn = document.createElement('BUTTON')
+    closeModalBtn.textContent = 'X';
+    closeModalBtn.classList.add('btn-close');
+    closeModalBtn.onclick = closeModal;
+    
+    modal.appendChild(image);
+    modal.appendChild(closeModalBtn);
+
+    const body = document.querySelector('body');
+    body.classList.add('overflow-hidden');
+
+    body.appendChild(modal);
+}
+
+function closeModal() {
+    const modal = document.querySelector('.modal');
+    modal.classList.add('fade-out');
+    setTimeout(() => {
+        modal?.remove();
+
+        const body = document.querySelector('body');
+        body.classList.remove('overflow-hidden');
+    }, 500);
+
 }
